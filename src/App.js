735a02e74eb0d64b8690f77dc565a1e087a43754
DEVMGO <devgolmohammadi@gmail.com>
2021-10-15 11:10:53 +0330

    upadate project todolist

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import TypeTodo from "./components/TypeTodo";
 import Todo from "./components/Todo";
-import "./assets/Styles/index.scss";
 import { useState } from "react";
 
 const App = () => {
 
   const [todoList, setTodoList] = useState([
-    {id:0, title: 'امیر' ,text:'بازید از محیط کار و مصاحبه'}
+    { id: 0, title: 'امیر', text: 'بازید از محیط کار و مصاحبه' }
   ]);
 
   return (
-    <main>
-      <div className="container">
-        <div className="todo-main row align-items-center">
-          <div className="col-md-6 h-50">
-            <TypeTodo todoList={todoList} setTodoList={setTodoList} />
-          </div>
-          <div className="todobar col-md-6 h-100">
-            {
-              todoList.map(item =>
+    <box>
+      <main>
+        <div className="app2 col-sm-12 fixed"></div>
+        <div className="container">
+          <div className="todo-main row align-items-center">
+            <div className="col-md-6 h-50 mt-3">
+              <TypeTodo todoList={todoList} setTodoList={setTodoList} />
+            </div>
+            <div className="todobar col-md-6 h-100">
+              {
+                todoList.map(item =>
                 (
-                  <Todo 
+                  <Todo
                     title={item.title}
                     text={item.text}
                     key={item.id}
@@ -29,11 +30,12 @@ const App = () => {
                     id={item.id}
                   />
                 ))
-            }
+              }
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </box>
   );
 }
 
